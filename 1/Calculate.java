8f8c06960ef4e7a483be1063d680150c9de2fa28
lucycore <1296720721@qq.com>
2020-02-26 23:22:26 +0800

    Java Hello world

---
+++ 1/Calculate.java
@@ -0,0 +1,10 @@
+
+
+public class Calculate {
+    public static void main(String[] args) {
+    	final double PI = 3.1415926;
+    	double r = 30;
+    	double area = PI * r * r;
+        System.out.println(area);
+    }
+}
